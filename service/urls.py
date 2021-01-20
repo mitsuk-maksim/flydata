46b984e3391a759fc84555a14406192a9eb97993
Maksim <mitsuk.maksim@gmail.com>
2021-01-21 00:17:30 +0300

    Поправленные url

--- service/urls.py
+++ service/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import UserComment, AllCommentsFromAllUsers, AllUserComments, CommentFromAllUsers
+from .views import UserComment
 
 urlpatterns = [
     path('user/comment/', UserComment.as_view(), name='user-comment'),
-    path('user/comment/<int:id>', AllUserComments.as_view(), name='user-comments'),
-    path('user/<int:pk>comment/', CommentFromAllUsers.as_view(), name='comment-all-users'),
-    path('user/<int:pk>comment/<int:id>', AllCommentsFromAllUsers.as_view(), name='all-comments-all-users'),
+    path('user/comment/<int:id>', UserComment.as_view(), name='user-comments'),
+    path('user/<int:pk>comment/', UserComment.as_view(), name='comment-all-users'),
+    path('user/<int:pk>comment/<int:id>', UserComment.as_view(), name='all-comments-all-users'),
 
 ]
